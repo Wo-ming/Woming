26fedeafeef89853a56e73ff4623b99ccdbec557
Yg-Hong <lion0077v@gmail.com>
2022-08-27 19:44:19 +0900

    Revert "[update]"
    This reverts commit 1bad300e6e3be516971b0671531e60e88933ef18.

--- woming_chatbot/test/model_ner_test.py
+++
@@ -1,9 +0,0 @@
-from utils.Preprocess import Preprocess
-from models.ner.NerModel import NerModel
-
-p = Preprocess(word2index_dic="../train_tools/dict/chatbot_dict.bin", userdic="../utils/user_dic.tsv")
-
-ner = NerModel(model_name="../models/ner/ner_model.h5", proprocess=p)
-query = "원문"
-predicts = ner.predict(query)
-print(predicts)
